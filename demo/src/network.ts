0e46ea8aec2b28e9d5bbf51ef8e314d5f3d788a6
Advaith Venkatakrishnan <advaith96@gmail.com>
2023-08-02 10:27:50 -0400

    added drag and drop with types

--- demo/src/network.ts
+++ demo/src/network.ts
@@ -1,5 +1,7 @@
 import {
+  D3DragEvent,
   SimulationNodeDatum,
+  drag,
   forceCenter,
   forceLink,
   forceManyBody,
@@ -25,12 +27,17 @@ export function getURLParameter(parameterName: string) {
   return searchParams.get(parameterName);
 }
 
-function runSimulation(svgId: string, height: number, width: number, graph: Graph, timestamp: number) {
-  console.log('timestamp', timestamp)
-  const { nodes: nodesData, links: linksData } =
-  graph;
+function runSimulation(
+  svgId: string,
+  height: number,
+  width: number,
+  graph: Graph,
+  timestamp: number
+) {
+  console.log("timestamp", timestamp);
+  const { nodes: nodesData, links: linksData } = graph;
 
-  const svg = select(svgId)
+  const svg = select(svgId);
 
   const links = svg
     .selectAll(".link")
@@ -48,7 +55,33 @@ function runSimulation(svgId: string, height: number, width: number, graph: Grap
     .attr("class", "node")
     .attr("id", (d) => d.id)
     .attr("r", 10)
-    .attr("data-timestamp", timestamp);
+    .attr("data-timestamp", timestamp)
+    .call(
+      drag<any, (typeof nodesData)[number]>()
+        .on("start", function (event: D3DragEvent<any, any, any>) {
+          if (!event.active) simulation.alphaTarget(0.3).restart();
+          select(this).raise().attr("stroke", "black");
+        })
+        .on("drag", function (event: D3DragEvent<any, any, any>, d: any) {
+          // select(this)
+          //   .attr("cx", (d.x = event.x))
+          //   .attr("cy", (d.y = event.y));
+          d.fx = event.x;
+          d.fy = event.y;
+        })
+        .on("end", function (event: D3DragEvent<any, any, any>, d: any) {
+          if(!d.fixed){
+            d.fx = event.x;
+            d.fy = event.y;
+            d.fixed = true;
+          } else {
+            d.fx = null;
+            d.fy = null;
+            d.fixed = false;
+          }
+          // select(this).attr("stroke", null);
+        })
+    );
 
   function ticked() {
     nodes
@@ -62,7 +95,7 @@ function runSimulation(svgId: string, height: number, width: number, graph: Grap
       .attr("y2", (d: any) => d.target.y);
   }
 
-  forceSimulation(nodesData.filter((node) => node.timestamp === timestamp))
+  const simulation = forceSimulation(nodesData.filter((node) => node.timestamp === timestamp))
     .force("charge", forceManyBody().strength(-20))
     .force("center", forceCenter(height / 2, width / 2))
     .force(
@@ -79,34 +112,33 @@ function runSimulation(svgId: string, height: number, width: number, graph: Grap
 }
 
 // Set up the D3 force simulation
-export function setupNetwork(element: string, playButton: string, max_timestamps: number) {
+export function setupNetwork(
+  element: string,
+  playButton: string,
+  max_timestamps: number
+) {
   const height = 300;
   const width = 300;
 
   const timestampParam = getURLParameter("timestamp");
   const timestamp = timestampParam ? parseInt(timestampParam) : 1;
 
-
-
   select(playButton).on("click", (d) => {
     console.log("Playing Animation");
-    let index = timestamp
+    let index = timestamp;
     setInterval(function () {
       if (index < max_timestamps) {
         index++;
       } else {
         index = 0;
       }
-      runSimulation("#prerender-canvas", height, width, staticGraph, index)
+      runSimulation("#prerender-canvas", height, width, staticGraph, index);
       console.log("Playing...");
 
-      select("#timeslider")
-        .attr('value', index)
+      select("#timeslider").attr("value", index);
 
-      select("#currentTimestampLabel")
-        .text(index)
+      select("#currentTimestampLabel").text(index);
     }, 1000);
-    // window.location.href = `/?timestamp=${d.target.value}`;
   });
 
   const svg = select(element)
@@ -114,53 +146,9 @@ export function setupNetwork(element: string, playButton: string, max_timestamps
     .attr("height", height)
     .attr("width", width)
     .style("border", "1px solid black")
-    .attr('id', 'prerender-canvas')
+    .attr("id", "prerender-canvas");
 
-  // const links = svg
-  //   .append("g")
-  //   .selectAll(".link")
-  //   .data(linksData.filter((link) => link.timestamp === timestamp))
-  //   .join("line")
-  //   .attr("class", "link")
-  //   .attr("stroke", "black")
-  //   .attr("fill", "none")
-  //   .attr("data-timestamp", timestamp);
-
-  // const nodes = svg
-  //   .selectAll(".node")
-  //   .data(nodesData.filter((node) => node.timestamp === timestamp))
-  //   .join("circle")
-  //   .attr("class", "node")
-  //   .attr("id", (d) => d.id)
-  //   .attr("r", 10)
-  //   .attr("data-timestamp", timestamp);
-
-  // function ticked() {
-  //   nodes
-  //     .attr("cx", (d: (typeof nodesData)[number]) => d?.x || 0)
-  //     .attr("cy", (d: (typeof nodesData)[number]) => d?.y || 0);
-
-  //   links
-  //     .attr("x1", (d: any) => d.source.x)
-  //     .attr("y1", (d: any) => d.source.y)
-  //     .attr("x2", (d: any) => d.target.x)
-  //     .attr("y2", (d: any) => d.target.y);
-  // }
-
-  // forceSimulation(nodesData.filter((node) => node.timestamp === timestamp))
-  //   .force("charge", forceManyBody().strength(-20))
-  //   .force("center", forceCenter(height / 2, width / 2))
-  //   .force(
-  //     "link",
-  //     forceLink(linksData.filter((link) => link.timestamp === timestamp))
-  //       .id((d: any) => d.id)
-  //       .distance(100)
-  //   )
-  //   .on("tick", ticked)
-  //   .on("end", function () {
-  //     const customEvent = new Event("simulationEnd");
-  //     document.dispatchEvent(customEvent);
-  //   });
+  runSimulation("#prerender-canvas", height, width, staticGraph, timestamp);
 
   return svg;
 }
